1bb9b3a3da13fcff046d31829043fdd26bb9caca
Carlos V <carlosdvp.tx@gmail.com>
2019-08-06 22:06:58 -0400

    created the function to consume the api

---
+++ app.js
@@ -0,0 +1,44 @@
+
+// Dependencies
+
+const axios = require('axios');
+const PORT = process.env.PORT || 5000
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+// a simple express server
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'public/index.html')))
+.get('/', (req,res) => res.sendFile(path.join(__dirname, './app.js')))
+.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(PORT, () => console.log(`Chuck Node-bot is listening on port ${PORT}`));
+
+
+// the Chucck Norris API random quote generator
+// const chuckAPI = 'https://api.chucknorris.io/jokes/random';
+const chuckAPI = 'http://api.icndb.com/jokes/random';
+
+
+// Start up message
+console.log('Chuck-bot is online, Human');
+
+// using axios to GET the data and post it to Twitter with Twit
+function getChuckQuote() {
+  axios.get(chuckAPI)
+  .then(function (response) {
+  	const data = response.data.value.joke;
+    console.log(data);
+    // T.post('statuses/update', {status: data});
+    return data;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+getChuckQuote();
+
+// automating the tweeting, one tweet every 10 seconds
+setInterval(getChuckQuote, 1000*10);
